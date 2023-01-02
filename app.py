853c9386c475702f3118de8f981c96638a51f1b7
maya8237 <maya.yellin@gmail.com>
2023-01-02 13:20:30 +0200

    Google Authentication

--- app.py
+++ app.py
@@ -1,8 +1,10 @@
+# Python standard libraries
 import json
 import os
 import sqlite3
 
-from flask import Flask, render_template, redirect, request, url_for
+# Third-party libraries
+from flask import Flask, redirect, request, url_for
 from flask_login import (
     LoginManager,
     current_user,
@@ -10,22 +12,73 @@ from flask_login import (
     login_user,
     logout_user,
 )
-
 from oauthlib.oauth2 import WebApplicationClient
 import requests
 
+# Internal imports
 from db import init_db_command
 from user import User
 
+# Configuration
 GOOGLE_CLIENT_ID = os.environ.get("GOOGLE_CLIENT_ID", None)
-GOOGLE_CLIENT_ID = os.environ.get("GOOGLE_CLIENT_SECRET", None)
-GOOGLE_DISCOVERY_URL = "https://accounts.google.com/.well-known/openid-configuration"
+GOOGLE_CLIENT_SECRET = os.environ.get("GOOGLE_CLIENT_SECRET", None)
+GOOGLE_DISCOVERY_URL = (
+    "https://accounts.google.com/.well-known/openid-configuration"
+)
+
+# Flask app setup
 app = Flask(__name__)
+app.secret_key = os.environ.get("SECRET_KEY") or os.urandom(24)
 
-@app.route('/')
+# User session management setup
+# https://flask-login.readthedocs.io/en/latest
+login_manager = LoginManager()
+login_manager.init_app(app)
+
+# Naive database setup
+try:
+    init_db_command()
+except sqlite3.OperationalError:
+    # Assume it's already been created
+    pass
+
+# OAuth 2 client setup
+client = WebApplicationClient(GOOGLE_CLIENT_ID)
+
+# Flask-Login helper to retrieve a user from our db
+@login_manager.user_loader
+def load_user(user_id):
+    return User.get(user_id)
+
+@app.route("/")
 def index():
-    return render_template('tba.html')
+    if current_user.is_authenticated:
+        return (
+            "<p>Hello, {}! You're logged in! Email: {}</p>"
+            "<div><p>Google Profile Picture:</p>"
+            '<img src="{}" alt="Google profile pic"></img></div>'
+            '<a class="button" href="/logout">Logout</a>'.format(
+                current_user.name, current_user.email, current_user.profile_pic
+            )
+        )
+    else:
+        return '<a class="button" href="/login">Google Login</a>'
 
+    def get_google_provider_cfg():
+        return requests.get(GOOGLE_DISCOVERY_URL).json()
 
-if __name__ == '__main__':
-    app.run(host='0.0.0.0', port = 5000)
+    @app.route("/login")
+    def login():
+        # Find out what URL to hit for Google login
+        google_provider_cfg = get_google_provider_cfg()
+        authorization_endpoint = google_provider_cfg["authorization_endpoint"]
+
+        # Use library to construct the request for Google login and provide
+        # scopes that let you retrieve user's profile from Google
+        request_uri = client.prepare_request_uri(
+            authorization_endpoint,
+            redirect_uri=request.base_url + "/callback",
+            scope=["openid", "email", "profile"],
+        )
+        return redirect(request_uri)
+
