49facdc2a9759d0cbf0233602aeb7c28836c7fee
maya8237 <maya.yellin@gmail.com>
2023-01-02 13:21:28 +0200

    Google Authentication

--- app.py
+++ app.py
@@ -64,21 +64,25 @@ def index():
     else:
         return '<a class="button" href="/login">Google Login</a>'
 
-    def get_google_provider_cfg():
-        return requests.get(GOOGLE_DISCOVERY_URL).json()
+def get_google_provider_cfg():
+    return requests.get(GOOGLE_DISCOVERY_URL).json()
 
-    @app.route("/login")
-    def login():
-        # Find out what URL to hit for Google login
-        google_provider_cfg = get_google_provider_cfg()
-        authorization_endpoint = google_provider_cfg["authorization_endpoint"]
+@app.route("/login")
+def login():
+    # Find out what URL to hit for Google login
+    google_provider_cfg = get_google_provider_cfg()
+    authorization_endpoint = google_provider_cfg["authorization_endpoint"]
 
-        # Use library to construct the request for Google login and provide
-        # scopes that let you retrieve user's profile from Google
-        request_uri = client.prepare_request_uri(
-            authorization_endpoint,
-            redirect_uri=request.base_url + "/callback",
-            scope=["openid", "email", "profile"],
-        )
-        return redirect(request_uri)
+    # Use library to construct the request for Google login and provide
+    # scopes that let you retrieve user's profile from Google
+    request_uri = client.prepare_request_uri(
+        authorization_endpoint,
+        redirect_uri=request.base_url + "/callback",
+        scope=["openid", "email", "profile"],
+    )
+    return redirect(request_uri)
 
+@app.route("/login/callback")
+def callback():
+    # Get authorization code Google sent back to you
+    code = request.args.get("code")
